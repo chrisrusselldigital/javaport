ed7b24a8d6c9fbc24f5dcf136936c8964416aa47
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-13 15:43:40 +0100

    debugged script error in airportspec

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,11 +1,16 @@
 'use strict';
 
-describe('Plane', function() {
+describe('Plane', function(){
   var plane;
+  var airport;
+
   beforeEach(function() {
     plane = new Plane();
+    airport = jasmine.createSpyObj('airport', ['clearForLanding']);
   });
-  it('Can land at an airport', function() {
-    expect(plane.land).not.toBeUndefined()
+
+  it('can land at an airport', function() {
+    plane.land(airport);
+    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
   });
 });
